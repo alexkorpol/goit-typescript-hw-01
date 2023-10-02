ee292ba6a9a57ed48380cd447e4763cc4fb1f849
Alexander <alexkorpol1964@gmail.com>
2023-10-02 15:58:10 +0300

    fix

--- src/concatenation.ts
+++ src/concatenation.ts
@@ -1,6 +1,5 @@
 function concatenation(firstWord: string, secondWord: string) {
     console.log(`${firstWord} ${secondWord}`);
-    console.log(`${secondWord} ${firstWord}`);
 }
 
 export { concatenation };
