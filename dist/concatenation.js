41df7dfdf1668ad4a1a2b777f914a01781a3da7c
Alexander <alexkorpol1964@gmail.com>
2023-10-02 15:56:53 +0300

    begin to study TS

---
+++ dist/concatenation.js
@@ -0,0 +1,6 @@
+function concatenation(firstWord, secondWord) {
+    console.log(`${firstWord} ${secondWord}`);
+    console.log(`${secondWord} ${firstWord}`);
+}
+export { concatenation };
+//# sourceMappingURL=concatenation.js.map
---
+++ src/concatenation.ts
@@ -0,0 +1,6 @@
+function concatenation(firstWord: string, secondWord: string) {
+    console.log(`${firstWord} ${secondWord}`);
+    console.log(`${secondWord} ${firstWord}`);
+}
+
+export { concatenation };
