494d1828b6b7c5eac965ba9e0438eab4cb8f59aa
Alexander <alexkorpol1964@gmail.com>
2023-10-02 16:00:08 +0300

    fix 2

--- dist/concatenation.js
+++ dist/concatenation.js
@@ -1,6 +1,5 @@
 function concatenation(firstWord, secondWord) {
     console.log(`${firstWord} ${secondWord}`);
-    console.log(`${secondWord} ${firstWord}`);
 }
 export { concatenation };
 //# sourceMappingURL=concatenation.js.map
